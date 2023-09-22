56c609f97c385da1d5522650e6c79dd27b1eb4a7
Federico Fontana <fontana.federico1991@gmail.com>
2023-09-22 17:47:49 +0100

    README.rst doctests are now collected by pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from tradingenv.contracts import AbstractContract
+from datetime import datetime
 
 
 @pytest.fixture(autouse=True)
 def reset_global_attributes():
-    now_before = AbstractContract.now
-    AbstractContract.now = now_before  # before the test
+    AbstractContract.now = datetime.min  # before the test
     yield  # run the test
-    AbstractContract.now = now_before  # after the test
+    AbstractContract.now = datetime.min  # after the test
